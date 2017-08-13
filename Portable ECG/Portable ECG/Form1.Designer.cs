1df43840313845b0893887612200007da1dceffc
saikat007saha <saikat007saha@gmail.com>
2017-08-13 08:53:27 +0530

    First Commit (uploading code)

---
+++ Portable ECG/Portable ECG/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Portable_ECG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(93, 12);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 41);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "ECG";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(647, 377);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // pid
+            // 
+            this.pid.Location = new System.Drawing.Point(174, 14);
+            this.pid.Name = "pid";
+            this.pid.Size = new System.Drawing.Size(251, 20);
+            this.pid.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(447, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(583, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ECG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 430);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Portable ECG";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox pid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Portable ECG/Portable ECG/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.IO.Ports;
+
+namespace Portable_ECG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] filter = new int[40];
+        int[] graph = new int[100];
+        int index = -1, n = -98, xaxis = 0, count = 0, number, counter = 0, beats = 0, t = 0, m, s;
+        string line, path1, path2, now, time;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t++;
+            m = t / 60;
+            s = t - m * 60;
+            time = "";
+            if (m < 10)
+                time += "0" + m + ":";
+            else
+                time += m + ":";
+            if (s < 10)
+                time += "0" + s;
+            else
+                time += s;
+        }
+
+        Thread ecgRead;
+        Thread update;
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            index = -1; n = -98; xaxis = 0; count = 0; counter = 0; beats = 0;
+            filter = new int[40];
+            graph = new int[100];
+            now = DateTime.Now.ToString();
+            now = now.Replace('/', '.');
+            now = now.Replace(':', '.');
+            now = pid.Text + now;
+            path1 = now + ".txt";
+            path2 = now + "_filtered.txt";
+
+            using (File.Create(path1)) { }
+            //using (File.Create(path2)) { }
+            CheckForIllegalCrossThreadCalls = false;
+
+            m = DateTime.Now.Minute;
+            s = DateTime.Now.Second;
+            //timer1.Start();
+
+            ecgRead = new Thread(getValues);
+            //ecgRead.IsBackground = true;
+            ecgRead.Start();
+            start.Enabled = false;
+
+            update = new Thread(getECG);
+            update.IsBackground = true;
+            update.Start();
+
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (!start.Enabled)
+            {
+                ecgRead.Abort();
+                update.Abort();
+                ecgReading();
+            }
+            start.Enabled = true;
+
+            //MessageBox.Show(beats.ToString());
+            //timer1.Stop();
+            //t = 0;
+        }
+
+        private void fill(int value)
+        {
+            index++;
+            filter[index] = value;
+            if(index>0)
+            {
+                for(int k=1;k<=index;k++)
+                {
+                    if (Math.Abs(filter[k - 1] - filter[k]) < 100 && filter[k] < 700)
+                        filter[k - 1] = (filter[k - 1] + filter[k]) / 2;
+                }
+            }
+        }
+
+        private void shift(int value)
+        {
+            //write 1st value of filter[] array;
+            using (StreamWriter sw2 = File.AppendText(path2))
+            {
+                sw2.WriteLine(filter[0]);
+            }
+            updateChart(value);
+            Array.Copy(filter, 1, filter, 0, 39);
+            filter[39] = value;
+            for (int k = 1; k < index; k++)
+            {
+                if (Math.Abs(filter[k - 1] - filter[k]) < 100 && filter[k] < 700)
+                    filter[k - 1] = (filter[k - 1] + filter[k]) / 2;
+            }
+            if (filter[38] < 600 && filter[39] > 600)
+            {
+                //ecgReading(counter);
+                counter = 0;
+                beats++;
+            }
+        }
+
+        private void getECG()
+        {
+            while (true)
+            {
+                if (chart1.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { updatechart(); });
+                }
+                else
+                {
+                    //...
+                }
+                //Thread.Sleep(1);
+            }
+        }
+        private void updateChart(int value)
+        {
+            Array.Copy(graph, 1, graph, 0, 99);
+            graph[99] = value;
+            xaxis++;
+            //chart1.Series["ECG"].Points.Clear();
+            //for (int k = 0; k < 100; k++)
+            //    chart1.Series["ECG"].Points.AddXY(n + k, graph[k]);
+            if (xaxis == 100)
+            {
+                n = n + 100;
+                xaxis = 0;
+            }
+        }
+
+        private void updatechart()
+        {
+            //Array.Copy(graph, 1, graph, 0, 99);
+            //graph[99] = value;
+            //xaxis++;
+            chart1.Series["ECG"].Points.Clear();
+            for (int k = 0; k < 100; k++)
+                chart1.Series["ECG"].Points.AddXY(n + k, graph[k]);
+            //if (xaxis == 100)
+            //{
+            //    n = n + 100;
+            //    xaxis = 0;
+            //}
+            label2.Text = ((DateTime.Now.Minute * 60 + DateTime.Now.Second)-(m*60+ s)) / 60 + ":" + ((DateTime.Now.Minute * 60 + DateTime.Now.Second) - (m * 60 + s)) % 60;
+            //if(time!="")
+            //    label2.Text = time;
+        }
+
+        private void getValues()
+        {
+            //SerialPort myport = new SerialPort();
+            //myport.BaudRate = 9600;
+            //myport.PortName = "COM3";
+            //myport.DataBits = 8;
+            //myport.Parity = Parity.None;
+            //myport.StopBits = StopBits.One;
+            //myport.Handshake = Handshake.None;
+            //myport.DtrEnable = true;
+            //myport.ReceivedBytesThreshold = 4096;
+            //myport.Open();
+
+            string[] port = SerialPort.GetPortNames();
+
+            using(SerialPort myport = new SerialPort(port[0],9600))
+            {
+                myport.Open();
+                while (true)
+                {
+                    line = myport.ReadLine();
+                    if (Int32.TryParse(line, out number) && number > 100)
+                    {
+                        //if (count < 40)
+                        //{
+                        //    count++;
+                        //    fill(number);
+                        //}
+                        //else
+                        //    shift(number);
+                        using (StreamWriter sw1 = File.AppendText(path1))
+                        {
+                            sw1.WriteLine(number);
+                        }
+                        //counter++;
+                        updateChart(number);
+                    }
+                }
+            }
+
+            
+        }
+
+        private void ecgReading()
+        {
+            int[] a = new int[1];
+            int c = 0, value, i, b=0, beats = 0;
+            System.IO.StreamReader file = new System.IO.StreamReader(path1);
+            while ((line = file.ReadLine()) != null)
+            {
+                //MessageBox.Show(line);
+                Int32.TryParse(line, out value);
+                if (value > 10 && value < 1010)
+                {
+                    a[c] = value;
+                    c++;
+                    Array.Resize(ref a, a.Length + 1);
+                }
+            }
+            TextWriter tw = new StreamWriter(path1.Replace(".txt", ".dat"));
+            for (i = 0; i < c; i++)
+            {
+                tw.WriteLine(i + "," + a[i]);
+                if (a[i + 1] - a[i] > 120 && i - b > 5)
+                {
+                    beats++;
+                    b = i;
+                    //MessageBox.Show(b.ToString());
+                }
+            }
+            tw.Close();
+            MessageBox.Show("Total number of beats recorded: " + beats.ToString());
+            float x = 6000.0f / c;
+            x = beats * x;
+            int y = (int)Math.Round(x);
+            MessageBox.Show("Average heart beat: " + y + " bpm");
+        }
+    }
+}
